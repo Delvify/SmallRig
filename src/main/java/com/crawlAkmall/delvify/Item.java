31ea97f9ed9306b5bdb86ff97e5cdde03d26172b
jugs <jageshmaharjan@hotmail.com>
2018-11-18 17:49:49 +0800

    crawler

---
+++ src/main/java/com/crawlAkmall/delvify/Item.java
@@ -0,0 +1,31 @@
+package com.crawlAkmall.delvify;
+
+import java.util.ArrayList;
+
+public class Item {
+    public String itemTitle;
+    public String designer;
+    public String keyFeatures;
+    public String compatibility;
+    public String packageIncludes;
+    public String productDim;
+    public String netWeight;
+    public String packageSize;
+    public String materials;
+    public String price;
+    public ArrayList<String> urlLinks;
+
+    public Item(String itemTitle, String designer, String keyFeatures, String compatibility, String packageIncludes, String productDim, String netWeight, String packageSize, String materials, String price, ArrayList<String> urlLinks) {
+        this.itemTitle = itemTitle;
+        this.designer = designer;
+        this.keyFeatures = keyFeatures;
+        this.compatibility = compatibility;
+        this.packageIncludes = packageIncludes;
+        this.productDim = productDim;
+        this.netWeight = netWeight;
+        this.packageSize = packageSize;
+        this.materials = materials;
+        this.price = price;
+        this.urlLinks = urlLinks;
+    }
+}
---
+++ src/main/java/com/crawlAkmall/delvify/ReiCrawler.java
@@ -0,0 +1,94 @@
+package com.crawlAkmall.delvify;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReiCrawler {
+
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<String> smallrigUrlList = SmallRigUrlReader();
+        for (String urllink : smallrigUrlList)
+            crawler(urllink);
+    }
+
+    private static void crawler(String urllink) throws Exception {
+
+        JSONObject obj = new JSONObject();
+        Document document = Jsoup.connect(urllink)
+                .data("query", "Java")
+                .userAgent("Mozilla")
+                .cookie("auth", "tokens")
+                .timeout(30000)
+                .post();
+
+        StringBuilder description = new StringBuilder();
+        Elements desc_element = document.select("div.product-description-container.rte").select("p");
+        for (Element header : desc_element){
+            if (header.text().split(":").length == 2 ){
+                obj.put(header.text().split(":")[0], header.text().split(":")[1]);
+//                System.out.println(header.text().split(":")[0]);
+//                System.out.println(header.text().split(":")[1]);
+            }
+            if (header.text().split(":").length > 2){
+                for (Element sub_desc : header.select("span")){
+                    obj.put(sub_desc.text().split(":")[0], sub_desc.text().split(":")[1]);
+//                  System.out.println(sub_desc.text().split(":")[0]);
+//                  System.out.println(sub_desc.text().split(":")[1]);
+                }
+            }
+            if (header.text().split(":").length < 2){
+                description.append(" ").append(header.text().trim());
+            }
+            obj.put("description",description.toString());
+        }
+
+        Elements item_element = document.select("div.product-info-column");
+        String item_title = item_element.select("h1.product-title").text();
+        obj.put("item_title",item_title);
+        String item_price = item_element.select("div.product-price").select("div.product-price-line").text();
+        obj.put("item_price", item_price);
+
+        // Images url list
+        JSONArray jsonArray = new JSONArray();
+        List<String> imageURL = new ArrayList<String>();
+        Elements item_image_url = document.select("div.product-slides-wrap.has-multiple-images").select("a");
+        for (Element url : item_image_url){
+            jsonArray.add(url.attr("href"));
+            imageURL.add(url.attr("href"));
+        }
+        obj.put("imageUrl", jsonArray);
+
+        writeToFile(obj);
+    }
+
+    private static void writeToFile(JSONObject obj) throws Exception {
+        FileWriter fileWriter = new FileWriter("newproduct.json", true);
+        fileWriter.write(obj.toJSONString() + "\n");
+        fileWriter.close();
+    }
+
+    private static ArrayList<String> SmallRigUrlReader() {
+        ArrayList<String> urlList = new ArrayList<String>();
+        String filePath = "SmallRigUrlList";
+        File file = new File(filePath);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null){
+                urlList.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return urlList;
+    }
+}
