10f2c0a9e9deca677d6780a76ff5d28a58476351
jugs <jageshmaharjan@hotmail.com>
2018-12-04 10:07:31 +0800

    json data

--- src/main/java/crawlAkmall/delvify/io/AllScraper.java
+++ src/main/java/crawlAkmall/delvify/io/AllScraper.java
@@ -10,20 +10,23 @@ public class AllScraper {
 
     public static void main(String[] args) throws Exception{
 
-        String mainUrl = "https://www.smallrig.com/cages/?sort=featured&page=7";
-        JSONObject obj = new JSONObject();
-        Document document = Jsoup.connect(mainUrl)
-                .data("query", "Java")
-                .userAgent("Mozilla")
-                .cookie("auth", "tokens")
-                .timeout(30000)
-                .post();
+        for (int i=1; i<=7; i++)
+        {
+            String mainUrl = "https://www.smallrig.com/cages/?sort=featured&page=" + i;
+            JSONObject obj = new JSONObject();
+            Document document = Jsoup.connect(mainUrl)
+                    .data("query", "Java")
+                    .userAgent("Mozilla")
+                    .cookie("auth", "tokens")
+                    .timeout(30000)
+                    .post();
 
 
-        Elements elements = document.select("div.collection-product-index-column").select("article").select("div.product-item-details").select("a");
-        for (Element url : elements){
-            ReiCrawler.crawler(url.attr("href").toString());
-            System.out.println(url.attr("href").toString());
+            Elements elements = document.select("div.collection-product-index-column").select("article").select("div.product-item-details").select("a");
+            for (Element url : elements){
+                ReiCrawler.crawler(url.attr("href").toString());
+                System.out.println(url.attr("href").toString());
+            }
         }
     }
 }
